01e9594a81db953391de491b374295d8eb4bc40e
XinSida <168305273+XinSida@users.noreply.github.com>
2024-05-12 13:27:18 +0800

    Update file_rename.sh

--- MOENAS_PSA_k_1/LSM_MOENAS/file_rename.sh
+++ MOENAS_PSA_k_1/LSM_MOENAS/file_rename.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
-file_name_rename=`ls -l ./MOENAS_PSA_k_1/LSM_MOENAS/output/results/ | awk -F [0-9][0-9][0-9][0-9][0-9][0-9][0-9][0-9][0-9][0-9][0-9][0-9] '{print $1}' | awk -F :[0-9][0-9]\ _ '{print $2}' | sed s/-//g`  #所有文件的文件名，去除开头的下划线
+file_name_rename=`ls -l ./MOENAS_PSA_k_1/LSM_MOENAS/output/results/ | awk -F [0-9][0-9][0-9][0-9][0-9][0-9][0-9][0-9][0-9][0-9][0-9][0-9] '{print $1}' | awk -F :[0-9][0-9]\ _ '{print $2}' | sed s/-//g` 
 file_name_rename_arr=($file_name_rename)
-rename_length=${#file_name_rename_arr[*]} #所有文件的数量
-result_file_name=`ls -l ./MOENAS_PSA_k_1/LSM_MOENAS/output/results/ | awk -F :[0-9][0-9]\  '{print $2}'` #最后多一个logo文件，保留开头下划线
+rename_length=${#file_name_rename_arr[*]} 
+result_file_name=`ls -l ./MOENAS_PSA_k_1/LSM_MOENAS/output/results/ | awk -F :[0-9][0-9]\  '{print $2}'` 
 result_file_name_arr=($result_file_name)
 arr_length=${#result_file_name_arr[*]}
 arr_index_last=`expr ${arr_length} - 1`
-unset result_file_name_arr[$arr_index_last]       #把最后的logo文件删了
+unset result_file_name_arr[$arr_index_last]      
 arr_length=${#result_file_name_arr[*]}
 k=0
 for i in ${result_file_name_arr[*]}
