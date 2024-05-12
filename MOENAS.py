49522fda1b145db5cacbf344cff23f38aa7279d3
XinSida <168305273+XinSida@users.noreply.github.com>
2024-05-12 19:48:11 +0800

    Add files via upload

--- MOENAS.py
+++ MOENAS.py
@@ -83,31 +83,101 @@ def evaluate_accuracy(x):
 
     pickle.dump(interconnection_1, interconnection_file)
 
-    # Parameter value range.
-    OUT_sample_defination = np.array([[0.9, 0.0], [0.9, 0.0], [0.9, 0.0], [0.9, 0.0], [0.9, 0.0]])
-    
     interconnection_file.close() 
 
     os.system('./MOENAS_PSA_k_1/LSM_MOENAS/talent_run.sh' + ' ' + '6000')
 
-    ##-----------------------------------------------------------------------------------------
-    ## main function
-    ##-----------------------------------------------------------------------------------------
-    num_example = int(sys.argv[1])
+    ##--------------------------------------------------------------------##
+    ## MLP training ##
+    ##--------------------------------------------------------------------##
+    
+    Y = np.load('./input_spike_record/train_labels_random_6000.npy')
+    train_data = LSM_state_vector_generate(is_trained=1)
+    train_data_rows, train_data_cols = train_data.shape
+    Y = Y - 1
+    train_labels = to_categorical(Y,10)
+
+    kfold = StratifiedKFold(n_splits=10, shuffle=True, random_state=1)
+
+    model = Sequential()
+    model.add(Dense(500, input_dim=train_data_cols, activation='relu'))
+    model.add(Dropout(0.5))
+    model.add(Dense(300, activation='relu'))
+    model.add(Dropout(0.5))
+    model.add(Dense(10, activation='softmax'))
+
+    lr_schedule = tf.keras.optimizers.schedules.ExponentialDecay(
+        initial_learning_rate=1e-2,
+        decay_steps=10000,
+        decay_rate=0.9)
+
+    optimizer = tf.keras.optimizers.SGD(learning_rate=lr_schedule)
+
+    model.compile(loss='categorical_crossentropy',
+                optimizer=optimizer,#sgd,
+                metrics=['accuracy'])
+
+    history = model.fit(train_data, train_labels,
+        epochs=300,
+        batch_size=128,
+        verbose=0)
+
+    ##---------------------------------------------------------------------------##
+    ## MLP testing ##
+    ##---------------------------------------------------------------------------##
+
+    Y = np.load('./input_spike_record/label_test_LSM_60000_70000.npy')
+    Y = Y - 1
+    test_labels = to_categorical(Y,10)
+    test_data = LSM_state_vector_generate(is_trained=0)
+                
+    predictlabel = model.predict(test_data)
+    score = model.evaluate(test_data, test_labels, batch_size=128)
+    
+    print('testing performance: ' + str(score[1]*100) + '%')
+
+    print('__________________time end____________________')
+    print(time.strftime('%Y-%m-%d %H:%M:%S',time.localtime(time.time())))
+    current_time = time.time()
+    expensive = current_time - old_time
+
+    with open("function_evaluation.txt", "a") as fw:
+        fw.write('evaluation time:'+str(expensive//60)+'分钟'+'\n')
+        fw.write('accuracy:'+str(score[1]*100)+'%'+'\n') 
+        fw.write('cost:'+str(round(x[0])+round(x[1])+round(x[2]))+'\n')
+        fw.write('actual_evaluations:'+str(actual_evaluations)+'\n') 
+        fw.write(str(x)+'\n'+'\n')
+        
+    command_delete = (
+        "rm -rf ./MOENAS_PSA_k_1/LSM_MOENAS/output/results/* && "
+        "rm -rf ./MOENAS_PSA_k_1/LSM_MOENAS/output/static_arrays/* && "
+        "rm -rf ./MOENAS_PSA_k_1/LSM_MOENAS/result/* && "
+        "rm -rf ./MOENAS_PSA_k_1/LSM_MOENAS/simulation_archive/random_connection/* && "
+        "rm -rf ./MOENAS_PSA_k_1/LSM_MOENAS/simulation_archive/result_testing/* && "
+        "rm -rf ./MOENAS_PSA_k_1/LSM_MOENAS/simulation_archive/result_training/* && "
+        "rm -rf ./MOENAS_PSA_k_1/LSM_MOENAS/simulation_archive/weight_learned/* && "
+        "rm -rf ./MOENAS_PSA_k_1/LSM_defination/*"
+    )
+    subprocess.run(command_delete, shell=True, check=True)
+
+    return score[1]
+
+def LSM_state_vector_generate(is_trained):
     using_resting = int(sys.argv[2])
     working_time = int(sys.argv[3])
     resting_time = int(sys.argv[4])
-
-    OUT_population = 10 #The numbers 0 through 9, totaling ten digits.
-
-    path_spike_record = './MOENAS_PSA_k_1/LSM_MOENAS/simulation_archive/result_training'
-    path_weight_learned = './MOENAS_PSA_k_1/LSM_MOENAS/simulation_archive/weight_learned'
-    
+    path_RC_defination = './MOENAS_PSA_k_1/LSM_defination'
     RCe_population_array = np.load(path_RC_defination + '/RCe_population_array.npy')
     RCi_population_array = np.load(path_RC_defination + '/RCi_population_array.npy')
-    
-    ##--------------LSM state process--------------------##
-    
+
+    if is_trained == 1:
+        num_example = int(sys.argv[1])
+        path_spike_record = './MOENAS_PSA_k_1/LSM_MOENAS/simulation_archive/result_training'
+    else:
+        num_example = 10000 
+        path_spike_record = './MOENAS_PSA_k_1/LSM_MOENAS/simulation_archive/result_testing'
+
+    path_weight_learned = './MOENAS_PSA_k_1/LSM_MOENAS/simulation_archive/weight_learned'
     RC_num = RCe_population_array.size + RCi_population_array.size
     connection_random_combine = np.array([], dtype = int)
     spike_normalized_combine = np.array([]) 
@@ -117,6 +187,11 @@ def evaluate_accuracy(x):
     RC_sampled_population = 0
     firing_rate_per_neuron_per_example = np.array([], dtype = float)
     error_during_training = np.array([], dtype = float)
+
+    # Parameter value range.
+    OUT_sample_defination = np.array([[0.9, 0.0], [0.9, 0.0], [0.9, 0.0], [0.9, 0.0], [0.9, 0.0]])
+
+    OUT_population = 10 #The numbers 0 through 9, totaling ten digits.
 
     for i in range(RC_num):#10 0~9
         if OUT_sample_defination[i//2][i%2] != 0:
@@ -203,7 +278,6 @@ def evaluate_accuracy(x):
                 spike_normalized_combine_resting = np.append(spike_normalized_combine_resting, spike_normalized_resting, axis = 1)
             combine_number += 1
 
-    iteration = 0
     connection_weight = np.random.uniform(0,1, (OUT_population, RC_sampled_population))
     if using_resting == 0:
         RC_state_vector = spike_normalized_combine
@@ -211,172 +285,8 @@ def evaluate_accuracy(x):
         RC_state_vector = spike_normalized_combine_working
     else:
         RC_state_vector = spike_normalized_combine_resting
-    
-    Y = np.load('./input_spike_record/train_labels_random_6000.npy')
-    train_data = RC_state_vector
-    train_data_rows, train_data_cols = train_data.shape
-    Y = Y - 1
-    train_labels = to_categorical(Y,10)
 
-    kfold = StratifiedKFold(n_splits=10, shuffle=True, random_state=1)
-
-    model = Sequential()
-    model.add(Dense(500, input_dim=train_data_cols, activation='relu'))
-    model.add(Dropout(0.5))
-    model.add(Dense(300, activation='relu'))
-    model.add(Dropout(0.5))
-    model.add(Dense(10, activation='softmax'))
-
-    lr_schedule = tf.keras.optimizers.schedules.ExponentialDecay(
-        initial_learning_rate=1e-2,
-        decay_steps=10000,
-        decay_rate=0.9)
-
-    optimizer = tf.keras.optimizers.SGD(learning_rate=lr_schedule)
-
-    model.compile(loss='categorical_crossentropy',
-                optimizer=optimizer,#sgd,
-                metrics=['accuracy'])
-
-    history = model.fit(train_data, train_labels,
-        epochs=300,
-        batch_size=128,
-        verbose=0)
-
-    ##---------------------------------------------------------------------------##
-    ## testing ##
-    ##---------------------------------------------------------------------------##
-
-    num_example = 10000 
-    path_spike_record = './MOENAS_PSA_k_1/LSM_MOENAS/simulation_archive/result_testing'
-    path_weight_learned = './MOENAS_PSA_k_1/LSM_MOENAS/simulation_archive/weight_learned'
-    
-    ##--------------LSM state process--------------------##
-    
-    spike_normalized_combine = np.array([]) 
-    spike_normalized_combine_working = np.array([]) 
-    spike_normalized_combine_resting = np.array([]) 
-    combine_number = 0
-    RC_sampled_population = 0
-
-    for i in range(RC_num):
-        if OUT_sample_defination[i//2][i%2] != 0:
-            if i % 2 == 0:
-                RC_population = RCe_population_array[i // 2]
-            else:
-                RC_population = RCi_population_array[i // 2]
-            RC_sampled_population += RC_population
-            
-            if i == 0:
-                spike_index_file = path_spike_record + '/dynamic_array_spikemonitor_i_'
-                spike_time_file = path_spike_record + '/dynamic_array_spikemonitor_t_'
-            else:
-                spike_index_file = path_spike_record + '/dynamic_array_spikemonitor_' + str(i) + '_i_'
-                spike_time_file = path_spike_record + '/dynamic_array_spikemonitor_' + str(i) + '_t_'
-            spike_monitor_index = np.fromfile(spike_index_file, dtype = np.int32, count = -1, sep = "" )
-            spike_monitor_time = np.fromfile(spike_time_file, dtype = np.float64, count = -1, sep = "")
-            if len(spike_monitor_index) != len(spike_monitor_time):
-                raise Exception('Spike records may not read properly!')
-            
-            print('firing rate for per neuron in neuron group' + str(i) + ' : ' + str(float(len(spike_monitor_index))/RC_population/num_example) + '/(neuron * example)')
-
-            spike_counter = np.zeros((num_example, RC_population), dtype = int)
-            spike_counter_working = np.zeros((num_example, RC_population), dtype = int)
-            spike_counter_resting = np.zeros((num_example, RC_population), dtype = int)
-            spike_counter_one = np.zeros(RC_population, dtype = int)
-            spike_counter_one_working = np.zeros(RC_population, dtype = int)
-            spike_counter_one_resting = np.zeros(RC_population, dtype = int)
-            working_time_per_example = working_time * 0.001  # second
-            resting_time_per_example = resting_time * 0.001  # second
-            time_per_example = working_time_per_example + resting_time_per_example
-            passed_time_one = time_per_example
-            number_example = 0
-            for j, item in enumerate(spike_monitor_time):
-                if item > passed_time_one:
-                    number_example = int(item/time_per_example) - 1
-                    spike_counter[number_example, :] = spike_counter_one 
-                    spike_counter_working[number_example, :] = spike_counter_one_working 
-                    spike_counter_resting[number_example, :] = spike_counter_one_resting
-                    spike_counter_one = np.zeros(RC_population)
-                    spike_counter_one_working = np.zeros(RC_population)
-                    spike_counter_one_resting = np.zeros(RC_population)
-                    passed_time_one += time_per_example
-            
-                spike_counter_one[spike_monitor_index[j]] += 1
-                if item < (passed_time_one - resting_time_per_example):
-                    spike_counter_one_working[spike_monitor_index[j]] += 1
-                else:
-                    spike_counter_one_resting[spike_monitor_index[j]] += 1
-                
-            number_example = num_example - 1 
-            spike_counter[number_example, :] = spike_counter_one 
-            spike_counter_working[number_example, :] = spike_counter_one_working
-            spike_counter_resting[number_example, :] = spike_counter_one_resting 
-
-            spike_counter_max = spike_counter.max(1).reshape(num_example, 1)
-            spike_counter_max_working = spike_counter_working.max(1).reshape(num_example, 1)
-            spike_counter_max_resting = spike_counter_resting.max(1).reshape(num_example, 1)
-
-            spike_normalized = spike_counter * 1.0 / spike_counter_max
-            spike_normalized_working = spike_counter_working * 1.0 / spike_counter_max_working
-            spike_normalized_resting = spike_counter_resting * 1.0 / spike_counter_max_resting
-
-            spike_normalized[np.isnan(spike_normalized)] = 0.0
-            spike_normalized_working[np.isnan(spike_normalized_working)] = 0.0
-            spike_normalized_resting[np.isnan(spike_normalized_resting)] = 0.0
-            if combine_number == 0:
-                spike_normalized_combine = spike_normalized
-                spike_normalized_combine_working = spike_normalized_working
-                spike_normalized_combine_resting = spike_normalized_resting
-            else:
-                spike_normalized_combine = np.append(spike_normalized_combine, spike_normalized, axis = 1)
-                spike_normalized_combine_working = np.append(spike_normalized_combine_working, spike_normalized_working, axis = 1)
-                spike_normalized_combine_resting = np.append(spike_normalized_combine_resting, spike_normalized_resting, axis = 1)
-            combine_number += 1
-
-    if using_resting == 0:
-        RC_state_vector = spike_normalized_combine
-    elif using_resting == 1:
-        RC_state_vector = spike_normalized_combine_working
-    else:
-        RC_state_vector = spike_normalized_combine_resting
-    
-    Y = np.load('./input_spike_record/label_test_LSM_60000_70000.npy')
-    Y = Y - 1
-    test_labels = to_categorical(Y,10)
-    test_data = RC_state_vector
-                
-
-    predictlabel = model.predict(test_data)
-    score = model.evaluate(test_data, test_labels, batch_size=128)
-    
-    print('testing performance: ' + str(score[1]*100) + '%')
-
-    print('__________________time end____________________')
-    print(time.strftime('%Y-%m-%d %H:%M:%S',time.localtime(time.time())))
-    current_time = time.time()
-    expensive = current_time - old_time
-
-    with open("function_evaluation.txt", "a") as fw:
-        fw.write('evaluation time:'+str(expensive//60)+'分钟'+'\n')
-        fw.write('accuracy:'+str(score[1]*100)+'%'+'\n') 
-        fw.write('cost:'+str(round(x[0])+round(x[1])+round(x[2]))+'\n')
-        fw.write('actual_evaluations:'+str(actual_evaluations)+'\n') 
-        fw.write(str(x)+'\n'+'\n')
-        
-    command_delete = (
-        "rm -rf ./MOENAS_PSA_k_1/LSM_MOENAS/output/results/* && "
-        "rm -rf ./MOENAS_PSA_k_1/LSM_MOENAS/output/static_arrays/* && "
-        "rm -rf ./MOENAS_PSA_k_1/LSM_MOENAS/result/* && "
-        "rm -rf ./MOENAS_PSA_k_1/LSM_MOENAS/simulation_archive/random_connection/* && "
-        "rm -rf ./MOENAS_PSA_k_1/LSM_MOENAS/simulation_archive/result_testing/* && "
-        "rm -rf ./MOENAS_PSA_k_1/LSM_MOENAS/simulation_archive/result_training/* && "
-        "rm -rf ./MOENAS_PSA_k_1/LSM_MOENAS/simulation_archive/weight_learned/* && "
-        "rm -rf ./MOENAS_PSA_k_1/LSM_defination/*"
-    )
-    subprocess.run(command_delete, shell=True, check=True)
-
-    return score[1]
+    return RC_state_vector
 
 surrogate_accuracy = SGDRegressor(max_iter=1000, tol=1e-3)
 feature_scaler = StandardScaler()
